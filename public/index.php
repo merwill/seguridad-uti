78009f270487a44459b225d2bd4ab1652eab93f9
Wilmer Alvarez GITHUB <wilmercharango@gmail.com>
2017-05-06 02:51:49 -0400

    carga al 06-05-2017 22222

--- public/index.php
+++ public/index.php
@@ -10,7 +10,7 @@ if (php_sapi_name() === 'cli-server' && is_file(__DIR__ . parse_url($_SERVER['RE
     return false;
 }
 
-// wilmer
+// wilmer alvarez
 // Setup autoloading
 require 'init_autoloader.php';
 
